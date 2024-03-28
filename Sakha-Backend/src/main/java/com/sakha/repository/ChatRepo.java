1ff4e62bc2aec203ca6bc71b5780f0de0d6c32df
Arpita Mittal <85882229+arpita010@users.noreply.github.com>
2024-03-28 20:37:59 +0530

    Update ChatRepo.java

--- Sakha-Backend/src/main/java/com/sakha/repository/ChatRepo.java
+++ Sakha-Backend/src/main/java/com/sakha/repository/ChatRepo.java
@@ -4,5 +4,9 @@ import com.sakha.entity.Chat;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ChatRepo extends CrudRepository<Chat, Long> {}
+public interface ChatRepo extends CrudRepository<Chat, Long> {
+     List<Chat> findAllByUserId(Long userId);
+}
